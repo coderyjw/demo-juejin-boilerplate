4b9f3e8edf29d0ca260774e9d6564c1453e71629
叶继伟 <729721503@qq.com>
2022-04-20 02:11:53 +0800

    权限管理

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import store from "./store";
 import intersect from "./directive/intersect";
 import router from "./router";
 import { init as themeInit } from "./config/theme";
+import { init as permissionInit } from "./config/permission";
 
 Vue.directive("intersect", intersect);
 themeInit();
+permissionInit();
 
 const app = new Vue({
   store,
--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import { routes as topic } from "./module/topic/router";
+import store from "./store";
+import { compose } from "./util/compose";
 
 Vue.use(VueRouter);
+import { PERMISSION_MAP, getPermissionByRole } from "./config/permission";
+
+const getRole = () => store.state.user.role;
+const getPermission = (permission) =>
+  compose((obj) => obj[permission], getPermissionByRole, getRole)();
 
 export default new VueRouter({
   routes: [
@@ -10,6 +17,19 @@ export default new VueRouter({
     {
       path: "/",
       redirect: "/hot",
+    },
+    {
+      name: "about",
+      path: "/about",
+      component: () => import("./views/UAbout.vue"),
+      beforeEnter(to, from, next) {
+        getPermission(PERMISSION_MAP.ABOUT_PAGE) ? next() : next("403");
+      },
+    },
+    {
+      name: "403",
+      path: "/403",
+      component: () => import("./views/403.vue"),
     },
   ],
 });
